b30b05798e8fc507a62f090b742b78c8ffaaae6a
nihal t n <nihaltn0@gmail.com>
2021-06-26 21:49:06 +0530

    ui improvement

--- app/src/main/java/com/example/myapp/phone_verifiation.java
+++ app/src/main/java/com/example/myapp/phone_verifiation.java
@@ -2,8 +2,14 @@ package com.example.myapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -124,7 +130,9 @@ public class phone_verifiation extends AppCompatActivity {
                 verify(mVerificationId,otp.getText().toString());
             }
         });
+
     }
+
     @Override
     public void onStart()
     {
